f3f1f72e8e7cb6bc8f4ef43e8c8f6b6300d1457c
VGeorgian <voda_georgian15@yahoo.com>
2023-04-01 14:04:35 +0300

    Lucru login + servicii

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,16 +1,36 @@
+// Angular
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+// Custom
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+
+// PrimeNG 
+import { ButtonModule } from 'primeng/button';
+import { AnimateModule } from 'primeng/animate';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { MessagesModule } from 'primeng/messages';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    ButtonModule,
+    AnimateModule,
+    InputTextModule,
+    PasswordModule,
+    MessagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
